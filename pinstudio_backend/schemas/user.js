b12bc2cbd28ae413e01d555fca5e0bd4a7ec3ab5
mazokujo <kabasele.jonathan@yahoo.fr>
2022-01-06 20:15:56 +0200

    creating Schemas in sanity and setting up tailwind css

---
+++ pinstudio_backend/schemas/user.js
@@ -0,0 +1,19 @@
+export default {
+    name: 'user',
+    title: 'User', //model
+    type: 'document',
+    fields: [
+        {
+            name: 'userName',
+            title: 'UserName', //model
+            type: 'string'
+
+        },
+        {
+            name: 'image',
+            title: 'Image', //model
+            type: 'string'
+
+        }
+    ]
+}
